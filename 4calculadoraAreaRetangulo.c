284d702de7f6d1d96eddf50a615c76bf5f0ab20b
juanmoises <juanmoises2003@yahoo.com>
2020-11-18 09:24:56 -0300

    creado 4calculadoraAreaRetangulo
    Creado código em C.

---
+++ 4calculadoraAreaRetangulo.c
@@ -0,0 +1,26 @@
+ /****************************************************************************
+ FileName:     4calculadoraAreaRetangulo
+ Company:      FATEC Santo Andre
+ Author:       Juan Moisés Carvallo Ferreira
+ Date:         18/11/2020
+ 
+ *****************************************************************************
+ File Description: Este programa calcula a area do quadrado.
+ Change History:
+ 1.0   18/11/2020  Versão inicial
+ 
+*****************************************************************************/
+#include <stdio.h>
+
+int main(void) {
+  float e_base,e_altura,s_area;
+
+  printf("Entre com o valor da base do retangulo\n");
+  scanf("%f", & e_base);
+  printf("Entre com o valor da altura do retangulo\n");
+  scanf("%f", & e_altura);
+  s_area=e_base*e_altura;
+  printf("O valor da area do retangulo = :%f\n",s_area);
+   
+  return 0;
+}
